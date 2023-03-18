92da5a2ad688bef92b3d0cdfcfd55c6359d333d8
inavernitskaya <inavernitskaya@gmail.com>
2023-03-18 09:22:10 +0200

    add loginSuccess

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,9 +1,37 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HelperUser extends HelperBase{
     public HelperUser(WebDriver wd) {
+
         super(wd);
+    }
+
+    public void openLoginForm(){
+        click(By.cssSelector("a[ng-reflect-router-link='login']"));
+    }
+
+    public void fillLoginForm(String email,String password) {
+
+        type(By.xpath("//input[@id='email']"), email);
+
+        type(By.xpath("(//input[@id='password'])[1]"),password);
+    }
+
+    public void submitLogin(){
+        click(By.cssSelector("button[type='submit']"));
+    }
+
+
+    public boolean isLogged() {
+        return isElementPresent(By.xpath("//button[.=' Logout ']"));
+
+    }
+
+    public void logout(){
+       click(By.xpath("//button[.=' Logout ']"));
     }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,34 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
 
-    @Test
-    public void test(){
+  @BeforeMethod
+    public void preCondition(){
 
+      if(app.getHelperUser().isLogged()){
+            app.getHelperUser().logout();
+      }
+    }
+
+    @Test
+    public void loginSuccess(){
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("inna_83@gmail.com","Aa13579$");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+    }
+
+    @Test
+    public void loginSuccessModel(){
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("inna_83@gmail.com","Aa13579$");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
     }
 }
