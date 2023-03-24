3221d0a40fcaa7f9229f472a690a76146510226b
inavernitskaya <inavernitskaya@gmail.com>
2023-03-25 00:10:01 +0300

    user model

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,9 +1,10 @@
 package manager;
 
+import models.User;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -11,55 +12,93 @@ import java.time.Duration;
 
 public class HelperUser extends HelperBase {
     public HelperUser(WebDriver wd) {
-
         super(wd);
     }
 
     public void openLoginForm() {
-        click(By.cssSelector("a[ng-reflect-router-link='login']"));
-        // click(By.cssSelector("(a[href^='/login']"));
-        //  click(By.xpath("a[text()=' Log in ']"));
+        //click(By.cssSelector("a[href='/login?url=%2Fsearch']"));
+        //click(By.cssSelector("a[href ^='/login']"));
+        click(By.xpath("//a[text()=' Log in ']"));
+
     }
 
     public void fillLoginForm(String email, String password) {
-        // type(By.xpath("//input[@id='email']"), email);
         type(By.id("email"), email);
-        // type(By.xpath("(//input[@id='password'])[1]"),password);
         type(By.id("password"), password);
     }
 
-    public void submitLogin() {
+    public void fillLoginForm(User user) {
+        type(By.id("email"), user.getEmail());
+        type(By.id("password"), user.getPassword());
+    }
+
+
+    public void submit() {
         click(By.cssSelector("button[type='submit']"));
     }
 
     public String getMessage() {
-        // WebElement element = wd.findElement(By.cssSelector(".dialog-container>h2"));
-        //String text = element.getText();
-        // return text;
+//        WebElement element = wd.findElement(By.cssSelector(".dialog-container>h2"));
+//        String  text = element.getText();
+//        return text;
 
         // wait
+
         WebDriverWait wait = new WebDriverWait(wd, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.visibilityOf(wd.findElement(By.cssSelector(".dialog-container"))));
 
-        // pause(2000);
+        // pause(8000);
         return wd.findElement(By.cssSelector(".dialog-container>h2")).getText();
     }
 
     public void closeWindow() {
+        if (isElementPresent(By.xpath("//button[text()='Ok']")))
+            click(By.xpath("//button[text()='Ok']"));
 
-        click(By.xpath("//button[text()='Ok']"));
     }
 
     public boolean isLogged() {
-
         return isElementPresent(By.xpath("//a[text()=' Logout ']"));
     }
 
     public void logout() {
-
-        click(By.xpath("//a[text()= ' Logout ']"));
+        click(By.xpath("//a[text()=' Logout ']"));
     }
 
+    public String getErrorText() {
+        String text = wd.findElement(By.cssSelector("div.error")).getText();
+        System.out.println(text);
 
+        return text;
+
+    }
+
+    public boolean isYallaButtonNotActive() {
+        boolean res = isElementPresent(By.cssSelector("button[disabled]"));
+        WebElement element = wd.findElement(By.cssSelector("button[type='submit']"));
+        boolean result = element.isEnabled();
+        return res && !result;
+    }
+
+    ///****************** Registration****************
+
+    public void openRegistrationForm() {
+        click(By.xpath("//a[text()=' Sign up ']"));
+    }
+
+    public void fillRegistrationForm(User user) {
+        type(By.id("name"), user.getFirstName());
+        type(By.id("lastName"), user.getLastName());
+        type(By.id("email"), user.getEmail());
+        type(By.id("password"), user.getPassword());
+    }
+
+    public void checkPolicy() {
+        // click(By.id("terms-of-use")); 0*0
+        //click(By.cssSelector("label[for='terms-of-use']"));
+        // document.querySelector('#terms-of-use').click();
+        JavascriptExecutor js = (JavascriptExecutor) wd;
+        js.executeScript("document.querySelector('#terms-of-use').click();");
+    }
 
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import models.User;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -19,36 +20,47 @@ public class LoginTests extends TestBase {
     public void loginSuccess() {
         app.getHelperUser().openLoginForm();
         app.getHelperUser().fillLoginForm("inna_83@gmail.com", "Aa13579$");
-        app.getHelperUser().submitLogin();
+        app.getHelperUser().submit();
         //Assert if element with text "Logged in success" is present
         Assert.assertEquals(app.getHelperUser().getMessage(), "Logged in success");
 
+    }
 
+    @Test
+    public void loginSuccess1() {
+          User user = new User().setEmail("inna_83@gmail.com").setPassword("Aa13579$");
+//        user.setEmail("inna_83@gmail.com");
+//        user.setPassword("Aa13579$");
+
+            app.getHelperUser().openLoginForm();
+            app.getHelperUser().fillLoginForm(user);
+            app.getHelperUser().submit();
+            //Assert if element with text "Logged in success" is present
+            Assert.assertEquals(app.getHelperUser().getMessage(),"Logged in success");
     }
 
     @Test
     public void loginSuccessModel() {
             app.getHelperUser().openLoginForm();
             app.getHelperUser().fillLoginForm("inna_83@gmail.com", "Aa13579$");
-            app.getHelperUser().submitLogin();
+            app.getHelperUser().submit();
             //Assert if element with text "Logged in success" is present
             Assert.assertEquals(app.getHelperUser().getMessage(), "Logged in success");
-
         }
-
         @Test
          public void loginWrongEmail() {
             app.getHelperUser().openLoginForm();
-            app.getHelperUser().fillLoginForm("inna_83@mail.com", "Aa13579$");
-            app.getHelperUser().submitLogin();
-            Assert.assertEquals(app.getHelperUser().getMessage(), "\"Login or Password incorrect\"");
+            app.getHelperUser().fillLoginForm("inna_83gmail.com", "Aa13579$");
+            app.getHelperUser().submit();
+            Assert.assertEquals(app.getHelperUser().getErrorText(),"It'snot look like email");
+            Assert.assertTrue(app.getHelperUser().isYallaButtonNotActive());
         }
 
         @Test
         public void loginWrongPassword() {
             app.getHelperUser().openLoginForm();
             app.getHelperUser().fillLoginForm("inna_83@gmail.com", "Aa13");
-            app.getHelperUser().submitLogin();
+            app.getHelperUser().submit();
             Assert.assertEquals(app.getHelperUser().getMessage(), "\"Login or Password incorrect\"");
         }
 
@@ -56,13 +68,13 @@ public class LoginTests extends TestBase {
         public void loginUnregisteredUser () {
             app.getHelperUser().openLoginForm();
             app.getHelperUser().fillLoginForm("pinokio@gmail.com", "Pn6474849$");
-            app.getHelperUser().submitLogin();
+            app.getHelperUser().submit();
             Assert.assertEquals(app.getHelperUser().getMessage(), "\"Login or Password incorrect\"");
         }
 
         @AfterMethod
         public void postCondition () {
-          app.getHelperUser().closeWindow();
+            app.getHelperUser().closeWindow();
        }
 
     }
